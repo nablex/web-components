ad1d4505a80e1a1e06cf69808fb0863d344559d5
Alexander Verbruggen <alex@nabu.be>
2019-10-21 16:54:51 +0200

    allow for empty strings

--- form/input/date/date.js
+++ form/input/date/date.js
@@ -251,7 +251,7 @@ Vue.component("n-form-date", {
 					nabu.utils.arrays.merge(messages, childMessages);
 				}
 			}
-			var parsed = value == null ? null
+			var parsed = !value ? null
 				: (this.parser ? this.parser(value) : new Date(value));
 			// it is "a" value but not a parseable value
 			if ((value != null && (parsed == null || !parsed.getTime))
