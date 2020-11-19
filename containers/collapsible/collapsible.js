683da28e7f2f5ec749cd88857463f5350b750882
Alexander Verbruggen <alex@nabu.be>
2020-11-19 22:08:48 +0100

    fixed collapsible button clicks toggle

--- containers/collapsible/collapsible.js
+++ containers/collapsible/collapsible.js
@@ -28,7 +28,8 @@ nabu.components.collapsible = Vue.component("n-collapsible", {
 	data: function () {
 		return {
 			show: false,
-			loading: false
+			loading: false,
+			toggleable: true
 		}
 	},
 	created: function() {
@@ -38,28 +39,30 @@ nabu.components.collapsible = Vue.component("n-collapsible", {
 	},
 	methods: {
 		toggle: function() {
-			if (!this.show) {
-				if (this.load) {
-					this.loading = true;
-					var self = this;
-					this.load().then(function() {
-						self.show = true;
-						self.loading = false;
-						self.$emit("show", self);
-					}, function() {
-						self.show = false;
-						self.loading = false;
-						self.$emit("hide", self);
-					});
+			if (this.toggleable) {
+				if (!this.show) {
+					if (this.load) {
+						this.loading = true;
+						var self = this;
+						this.load().then(function() {
+							self.show = true;
+							self.loading = false;
+							self.$emit("show", self);
+						}, function() {
+							self.show = false;
+							self.loading = false;
+							self.$emit("hide", self);
+						});
+					}
+					else {
+						this.show = true;
+						this.$emit("show", this);
+					}
 				}
 				else {
-					this.show = true;
-					this.$emit("show", this);
+					this.show = false;
+					this.$emit("hide", this);
 				}
-			}
-			else {
-				this.show = false;
-				this.$emit("hide", this);
 			}
 		}
 	}
